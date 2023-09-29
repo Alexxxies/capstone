732c770cb53d838335f40237e7c938a395b200aa
allysajane <allysajanecatibog@gmail.com>
2023-09-29 15:15:13 +0800

    Commit

---
+++ applicant/applicant.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Southern Jade Life Insurance</title>
+  <link rel="icon" href="../images/logajade.png" type="image/x-icon">
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css"
+  rel="stylesheet"
+/>
+<script src="https://kit.fontawesome.com/866d550866.js" crossorigin="anonymous"></script>
+
+ <!--JQuery-->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<link href="css/style.css" rel="stylesheet"/>
+
+<style>
+ 
+</style>
+
+
+</head> 
+<body>
+<nav class="navbar bg-body-tertiary shadow-none">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <img src="../images/logo-topnav.png" alt="Logo" width="270" height="50" class="d-inline-block align-text-top">
+        <div class="d-inline" style="margin-right:50px;">
+            <a href="#" style="color: black; margin-right: 30px;">Contact Us</a>
+            <a href="#" style="color: black; ">About Us</a>
+        </div>
+    </div>
+</nav>
+<div class="content">
+<div class="container m-6 justify-content-center">
+    <div class="row ">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="template/pictures/Southern Jade-modified.png" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <p>Marco Luis Hernandez</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <strong>FIRST SEMESTER AY 2021-2022</strong>
+            <P>College of Information and Computing Sciences - JPLPC Malvar</p>
+            <p>BS Information Technology</p>
+            <p>Service Management</p>
+            <strong>ENROLLED</strong>
+        </div>
+    </div>
+<br>
+
+    <div class="container-sm">
+    <i class="fa-solid fa-user"></i> 
+    Details </div>  
+</div>
+
+</div>
+   </body>
+</html>
---
+++ applicant/registration/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Southern Jade</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+
+  <body>
+    <div class="container form-container">     
+      <div class=" col-lg-12 mx-auto login-container ">
+            <div class="row form-header">
+                <div class="col-md-3 logocol">
+                  <img src="assets/images/logo-topnav.png" alt="">
+                </div>
+            </div>
+            <div class="row ">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="template/pictures/Southern Jade-modified.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-md-6">
+                          <br>
+                            <p>Marco Luis Hernandez</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                  <br>
+                    <strong>FIRST SEMESTER AY 2021-2022</strong>
+                    <P>College of Information and Computing Sciences - JPLPC Malvar</p>
+                    <p>BS Information Technology</p>
+                    <p>Service Management</p>
+                    <strong>ENROLLED</strong>
+                </div>
+            </div>
+            <div class="form-body">
+              <div class="form-title row">
+              <h4></h4>
+              </div>
+              <h4> Overview </h4>
+            </div>
+      </div>
+    </div>
+
+
+ <!--JQuery-->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"
+></script>
+
+
+    <script src="ph-address-selector.js"></script>
+
+  </body>
+  </html
